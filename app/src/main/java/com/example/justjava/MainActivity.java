1cd7b28f7875438fad15aba7022728b08aab3c7f
manish <shahmannish@gmail.com>
2019-05-28 04:41:37 +0530

    email Intent added , quantity increment decrement bug solved

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -31,16 +34,25 @@ public class MainActivity extends AppCompatActivity {
         double total_bill = calculatePrice(noOfCoffe, isWhipped, isTopping);
         String summary = orderSummary(total_bill, isWhipped, isTopping);
         displaySummary(summary);
+        String[] emailId = {"manishdear@gmail.com", "studmanishshah@gmail.com"};
+        String subject = "A coffe order from " + getName();
+        composeEmail(emailId, subject, summary);
     }
 
     private boolean isWhippedChecked(){
         CheckBox checkBox = findViewById(R.id.cb_wipped);
-        return checkBox.isChecked();
+        if (checkBox.isChecked())
+            return true;
+        else
+            return false;
     }
 
     private boolean isToppingChecked(){
         CheckBox checkBox = findViewById(R.id.cb_topping);
-        return checkBox.isChecked();
+        if (checkBox.isChecked())
+            return true;
+        else
+            return false;
     }
 
     private void orderSummaryHeading(String msg){
@@ -60,17 +72,37 @@ public class MainActivity extends AppCompatActivity {
         return total;
     }
 
-    public void increment(View view){
-       displayQuantity(++noOfCoffe);
+    /**
+     * increment
+     * @param view
+     */
+
+    public void increment(View view)  {
+        if(noOfCoffe==10)
+            Toast.makeText(this, "You cannot have more than 10 Coffees", Toast.LENGTH_SHORT).show();
+        else {
+            noOfCoffe++;
+            displayQuantity(noOfCoffe);
+        }
     }
 
-    public void decrement(View view){
-        displayQuantity(--noOfCoffe);
+    /**
+     * decrement
+     * @param view
+     */
+
+    public void decrement(View view) {
+        if(noOfCoffe==1)
+            Toast.makeText(this, "You cannot have less than 1 Coffees", Toast.LENGTH_SHORT).show();
+        else {
+            noOfCoffe--;
+            displayQuantity(noOfCoffe);
+        }
     }
 
     private void displayQuantity(int noOfCoffe){
         TextView quantity = findViewById(R.id.quantity_text_view);
-        quantity.setText(noOfCoffe);
+        quantity.setText("" +noOfCoffe);
     }
 
     private String getName(){
@@ -93,5 +125,17 @@ public class MainActivity extends AppCompatActivity {
         TextView order = findViewById(R.id.price_text_view);
         order.setText(summary);
     }
+
+    public void composeEmail(String[] addresses, String subject, String message) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
 
 }
